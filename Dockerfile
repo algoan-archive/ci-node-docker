0d45cc726305a6568443fe78121babbe9e6a09ff
Charles Coeurderoy <charles.coeurderoy@yelloan.com>
2018-04-06 11:08:29 +0200

    Bump node version to 8.11.1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:8.10.0
+FROM node:8.11.1
 MAINTAINER Yelloan "devteam@yelloan.com"
 
 ENV CLOUD_SDK_VERSION 183.0.0
--- pushToDockerHub.sh
+++ pushToDockerHub.sh
@@ -1,6 +1,6 @@
 export DOCKER_USERID=${DOCKER_USERID:-"yelloan"}
 export DOCKER_IMAGE=${DOCKER_IMAGE:-"node-docker"}
-export DOCKER_TAG=${DOCKER_TAG:-"9.3.0-183.0.0"}
+export DOCKER_TAG=lts
 docker build -t $DOCKER_IMAGE .
 docker tag $DOCKER_IMAGE $DOCKER_USERID/$DOCKER_IMAGE:$DOCKER_TAG
 docker push $DOCKER_USERID/$DOCKER_TAG
